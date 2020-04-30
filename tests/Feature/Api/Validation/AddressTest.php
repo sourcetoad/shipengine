231e48d11b57b565829776b8a0383a81dff35132
Nicholas Maurer <nico@sourcetoad.com>
2020-04-30 17:34:10 -0400

    SE refactor: remove mock data and add test styles.

--- tests/Feature/Api/Validation/AddressTest.php
+++ tests/Feature/Api/Validation/AddressTest.php
@@ -2,71 +2,31 @@
 
 namespace Feature\Api\Validation;
 
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Psr7\Response;
 use jsamhall\ShipEngine\Address\Address;
-use jsamhall\ShipEngine\Address\ArrayFormatter;
-use jsamhall\ShipEngine\ShipEngine;
-use jsamhall\ShipEngine\Address\Factory as AddressFactory;
-use jsamhall\ShipEngine\Exception;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
+use Tests\Mocks\Validation\AddressMock;
 
 class AddressTest extends \PHPUnit_Framework_TestCase
 {
 
     public function testValidateAddress()
     {
+        //Arrange
         $address = new Address();
-        $address->setAddressLine1("525 S WINCHESTER BLVD")
+        $address->setAddressLine1("525 S Winchester Blvd")
             ->setCityLocality("San Jose")
             ->setStateProvince("CA")
             ->setPostalCode("95128")
             ->setCountryCode("US")
             ->setAddressResidentialIndicator("unknown");
-        $addressResponse = [
-            [
-                "status" => "verified",
-                "original_address" => [
-                    "name"                          => null,
-                    "phone"                         => null,
-                    "company_name"                  => null,
-                    "address_line1"                 => "525 S Winchester Blvd",
-                    "address_line2"                 => null,
-                    "address_line3"                 => null,
-                    "city_locality"                 => "San Jose",
-                    "state_province"                => "CA",
-                    "postal_code"                   => "95128",
-                    "country_code"                  => "US",
-                    "address_residential_indicator" => "unknown"
-                ],
-                "matched_address" => [
-                    "name"                          => null,
-                    "phone"                         => null,
-                    "company_name"                  => null,
-                    "address_line1"                 => "525 S WINCHESTER BLVD",
-                    "address_line2"                 => "",
-                    "address_line3"                 => null,
-                    "city_locality"                 => "SAN JOSE",
-                    "state_province"                => "CA",
-                    "postal_code"                   => "95128-2537",
-                    "country_code"                  => "US",
-                    "address_residential_indicator" => "no"
-                ],
-                "messages" => []
-            ]
-        ];
-        $mock = new MockHandler([new Response(200, [], json_encode($addressResponse))]);
-        $mockStack = HandlerStack::create($mock);
-        $mockOptions = ['handler' => $mockStack];
+        $shipEngineMock = new AddressMock();
 
-        $addressFormatter = new ArrayFormatter();
+        $request = $shipEngineMock->mockValidationSuccess();
 
-        $shipEngineAddress = new ShipEngine("", $addressFormatter, $mockOptions);
+        //Act
+        $response = $request->validateAddress($address);
 
-        $response = $shipEngineAddress->validateAddress($address);
-
-        $this->assertSame($address->getAddressLine1(), $response->getMatchedAddress()->getAddressLine1());
+        //Assert
+        $this->assertSame(strtoupper($address->getAddressLine1()), $response->getMatchedAddress()->getAddressLine1());
+        $this->assertSame(strtoupper($address->getCityLocality()), $response->getMatchedAddress()->getCityLocality());
     }
 }
